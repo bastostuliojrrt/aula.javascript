41b399745560d05dc615cba7669f6015424a85d2
bastostuliojrrt <37836280+bastostuliojrrt@users.noreply.github.com>
2022-03-06 12:19:50 -0300

    Delete Estudos - casa directory

--- Estudos - casa/JS/nullUndefined.js
+++
@@ -1,8 +0,0 @@
-// Null é a ausência intencional de um valor
-var teste = null;
-
-// Undefined indica que a variável declarada não possui valor
-var teste2;
-
-console.log(teste);
-console.log(teste2);
