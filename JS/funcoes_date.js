cda26bf42c8c34885eacdcc792d7754f4e8856dd
Tulio Bastos <tulio.bastos@meta55-net.com.br>
2022-03-22 22:00:10 -0300

    Manipulação de datas

--- JS/funcoes_date.js
+++ JS/funcoes_date.js
@@ -1,3 +1,14 @@
 var data = new Date();
 
-document.write(data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear());
+// document.write(data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear());
+
+document.write(data.toString());
+document.write("<hr/>");
+
+document.write(data.toString());
+data.setDate(data.getDate() + 1); // Adicionando/subtraindo o valor da data
+document.write("<hr/>");
+
+document.write(data.toString());
+data.setMonth(data.getMonth() + 1); // Adicionando/subtraindo o valor do mês
+document.write("<hr/>");
